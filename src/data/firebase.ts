0efcdb6d5ce91090f913c4a083242b898aa0627f
Brian Lucas <bcl1713@gmail.com>
2022-05-03 14:59:00 +0200

    firebase initilization

---
+++ src/data/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyClnMR1bQIOTnd2tvOscRBS4Nyko1R9uis",
+  authDomain: "gitlab-pages-react-example.firebaseapp.com",
+  projectId: "gitlab-pages-react-example",
+  storageBucket: "gitlab-pages-react-example.appspot.com",
+  messagingSenderId: "513463229518",
+  appId: "1:513463229518:web:b69b772bc65fc3d40e371f",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
